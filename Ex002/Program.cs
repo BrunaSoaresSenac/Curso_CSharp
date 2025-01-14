e22122a2961bf9935abfd44b56465f69bc684ccd
Bruna <bruna.spaulino@senacsp.edu.br>
2025-01-13 22:06:42 -0300

    Adicionar arquivos de projeto.

---
+++ Ex002/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = -45;
+            float num = 4.58f;
+            char letra = 'c';
+            string nome = "Bruna";
+
+            Console.WriteLine("O número é: {0}.", numero);
+            Console.WriteLine("O valor é: R${0}.", num);
+            Console.WriteLine("A letra é: {0}.", letra);
+            Console.WriteLine("O nome é: {0}", nome);
+
+            Console.WriteLine("O número é {0}. O valor é R$ {1}. A letra é {2}. O nome é {3}.", numero, num, letra, nome);
+        }
+    }
+}
---
+++ Ex005/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Ex005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Digite um número para calcular a tabuada:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            num2 = num1 * 0;
+            Console.WriteLine("{0} X 0 = {1}", num1, num2);
+            num2 = num1 * 1;
+            Console.WriteLine("{0} x 1 = {1}", num1, num2);
+            num2 = num1 * 2;
+            Console.WriteLine("{0} x 2 = {1}", num1, num2);
+            num2 = num1 * 3;
+            Console.WriteLine("{0} x 3 = {1}", num1, num2);
+            num2 = num1 * 4;
+            Console.WriteLine("{0} x 4 = {1}", num1, num2);
+            num2 = num1 * 5;
+            Console.WriteLine("{0} x 5 = {1}", num1, num2);
+            num2 = num1 * 6;
+            Console.WriteLine("{0} x 6 = {1}", num1, num2);
+            num2 = num1 * 7;
+            Console.WriteLine("{0} x 7 = {1}", num1, num2);
+            num2 = num1 * 8;
+            Console.WriteLine("{0} x 8 = {1}", num1, num2);
+            num2 = num1 * 9;
+            Console.WriteLine("{0} x 9 = {1}", num1, num2);
+            num2 = num1 * 10;
+            Console.WriteLine("{0} x 10 = {1}", num1, num2);
+
+
+
+
+
+
+            int num;
+            Console.WriteLine("Digite um número para calcular a tabuada:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" {0} X 0 = {1}", num, num * 0);
+            Console.WriteLine(" {0} X 1 = {1}", num, num * 1);
+            Console.WriteLine(" {0} X 2 = {1}", num, num * 2);
+            Console.WriteLine(" {0} X 3 = {1}", num, num * 3);
+            Console.WriteLine(" {0} X 4 = {1}", num, num * 4);
+            Console.WriteLine(" {0} X 5 = {1}", num, num * 5);
+            Console.WriteLine(" {0} X 6 = {1}", num, num * 6);
+            Console.WriteLine(" {0} X 7 = {1}", num, num * 7);
+            Console.WriteLine(" {0} X 8 = {1}", num, num * 8);
+            Console.WriteLine(" {0} X 9 = {1}", num, num * 9);
+            Console.WriteLine(" {0} X 10 = {1}", num, num * 10);
+
+
+
+        }
+    }
+}
