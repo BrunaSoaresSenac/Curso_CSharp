3ed415d26093cfaec42988e5573b046d0c8b7c9d
Bruna <bruna.spaulino@senacsp.edu.br>
2025-01-15 21:28:12 -0300

    Novo exercício.

---
+++ Ex002/ex002/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome;
+        nome = "Bruna";
+        Console.WriteLine("Olá, {0}! Prazer em conhecê-lo(a).",nome);
+
+    }
+}
---
+++ Ex006/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Digite a primeira nota:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A média entre {0} e {1} é: {2}", num1, num2, (num1+num2)/2);
+
+
+
+            double n1, n2, media;
+
+            Console.Write("Digite a primeira nota: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (n1+n2)/2;
+
+            Console.WriteLine("Média: {0}", media);
+        }
+    }
+}
