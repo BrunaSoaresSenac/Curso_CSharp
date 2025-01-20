94c585b74a472518da0226cd568598c2f3e07015
Bruna <bruna.spaulino@senacsp.edu.br>
2025-01-20 20:00:44 -0300

    Adc novas infs.

--- Ex009/Program.cs
+++ Ex009/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Ex009
+﻿using System.ComponentModel.Design;
+
+namespace Ex009
 {
     internal class Program
     {
@@ -14,6 +16,15 @@
 
             Console.Write("Digite o valor do lado C: ");
             ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA+ladoB))
+            {
+                Console.WriteLine("Os valores informados formam um triângulo.");
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados não formam um triângulo.");
+            }
         }
     }
 }
