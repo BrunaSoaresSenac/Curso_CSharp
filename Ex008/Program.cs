d217802a54beb451f2d7f01f7cfb34d2b03a576f
Bruna <bruna.spaulino@senacsp.edu.br>
2025-01-20 19:41:03 -0300

    Exercício do teste do triângulo

---
+++ Ex008/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aviso;
+            double velocidade, multa;
+
+            Console.Write("Digite a velocidade: ");
+            velocidade = Convert.ToInt32(Console.ReadLine());
+
+            multa = (velocidade - 80) * 7;
+            multa = Convert.ToInt32(Console.ReadLine());
+
+            if (velocidade > 81)
+            {
+                Console.Write("Você foi multado! O valor da multa é de R$ {0}", multa);
+            }
+            else
+            {
+                Console.Write("Velocidade dentro do permitido");
+            }
+        }
+    }
+}
